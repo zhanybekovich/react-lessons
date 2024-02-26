3d81ed4dae48d8ca1ce6c675777ead08274071a3
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-02-26 21:22:14 +0600

    Lesson 1

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Hello my React APP</h1>
+      <Header />
+      <Header />
+      <h2>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat quas
+        ipsam suscipit fugit quos soluta possimus obcaecati, culpa officia
+        tenetur quidem a, maiores odit non illo at voluptatum? Impedit, esse!
+      </h2>
+      <Header />
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="#">Logo</a>
+
+        <ul>
+          <li>
+            <a href="#">Главная</a>
+          </li>
+          <li>
+            <a href="#">О нас</a>
+          </li>
+          <li>
+            <a href="#">Контакты</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
